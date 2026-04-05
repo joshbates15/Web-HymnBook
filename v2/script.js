aef8372bee551a64e468b0df6374f878c56e745b
joshbates15 <joshbates15@gmail.com>
2026-04-05 14:17:02 -0400

    Update script.js

--- v2/script.js
+++ v2/script.js
@@ -32,10 +32,10 @@ function togglePageVisibility(showPage, hidePage) {
     if (showPage === 'hymn-display-page') {
         // Shrink the title when viewing a hymn
         title.classList.remove('text-4xl', 'py-2');
-        title.classList.add('text-xl', 'py-1');
+        title.classList.add('text-sm', 'py-1');
     } else {
         // Grow the title back when on the keypad
-        title.classList.remove('text-xl', 'py-1');
+        title.classList.remove('text-sm', 'py-1');
         title.classList.add('text-4xl', 'py-2');
     }
     document.querySelector('.' + showPage).classList.remove('hidden');
